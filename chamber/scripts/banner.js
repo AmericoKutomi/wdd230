436f5a372e6c1ed53d69e47abf5cfd3577178e6d
AmericoKutomi <americo@embra.com.br>
2023-02-15 12:11:15 -0300

    Banner fitting

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -5,7 +5,7 @@ function showBanner() {
     const currentDate = new Date();
     dayOfTheWeek = currentDate.getDay();
     if (dayOfTheWeek >= 1 && dayOfTheWeek <= 3) {
-        banner.textContent = "All user are invited to attend the Chamber of Commerce meet and greet on Wednesday at 7:00 p.m.";
+        banner.textContent = "All users are invited to attend the Chamber of Commerce meet and greet on Wednesday at 7:00 p.m.";
         banner.style.display = "block";
     } else {
         banner.style.display = "none";
